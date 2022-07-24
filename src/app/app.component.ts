3613c7c18b47ef5bf0376c6087cc5db201ab2ddb
Chollada <54612005+por031hub@users.noreply.github.com>
2022-07-24 10:24:03 +0700

    uplaod

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { count } from 'rxjs';
 export class AppComponent {
   show_img : any;
   title = 'covid19.2';
-  files : any[]= [];
+  files : any
 
   imgPre:string = ''
 
@@ -21,25 +21,25 @@ export class AppComponent {
   
   myUploader(event : any) {
     try {
-      console.log(event);
+      this.files = event.files[0]
       this.show_img = event.files[0];
       const reader = new FileReader();
       
       reader.onload = e => this.show_img = reader.result;
-      reader.readAsDataURL(this.show_img);
-      this.getImage(this.show_img)  
+      reader.readAsDataURL(this.show_img); 
     } catch (error) {
     }
   }
 
-  private getImage(file:File){
+  public getImage(){
+    
+    
     const formData: FormData = new FormData();
-    formData.append('file', file, file.name);
+    formData.append('file',  this.files, this.files.name);
     this.http.post('http://127.0.0.1:5000/upload/image',formData).subscribe(data => {
       console.log(data)
       this.imgPre = 'http://127.0.0.1:5000/image/'+data
     })
-
   }
 
 }
