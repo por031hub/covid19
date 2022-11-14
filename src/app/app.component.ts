5e95abfba7c5478e0077a0c6575f0b0b759a54c2
Chollada <54612005+por031hub@users.noreply.github.com>
2022-11-14 17:48:34 +0700

    font

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,12 +22,10 @@ export class AppComponent {
     console.log('prdict');
     const formData: FormData = new FormData();
     formData.append('file',  this.files, this.files.name);
-    this.http.get('http://127.0.0.1:5000/predict').subscribe(data => {
+    this.http.get('http://203.150.243.147:5000/predict').subscribe(data => {
       console.log(data)
       this.result = data as string;
-      this.imgPre = 'http://127.0.0.1:5000/image/'+data
-
-     
+      this.imgPre = 'http://203.150.243.147:5000/image/'+data
     })
   }
   myUploader(event : any) {
@@ -35,23 +33,21 @@ export class AppComponent {
       this.files = event.files[0]
       this.show_img = event.files[0];
       const reader = new FileReader();
-      
+  
       reader.onload = e => this.show_img = reader.result;
       reader.readAsDataURL(this.show_img); 
       this.predict();
     } catch (error) {
     }
   }
-
-
   public getImage(){
     
     const formData: FormData = new FormData();
     formData.append('file',  this.files, this.files.name);
-    this.http.post('http://127.0.0.1:5000/upload/image',formData).subscribe(data => {
+    this.http.post('http://203.150.243.147:5000/upload/image',formData).subscribe(data => {
        console.log(data)
        this.result = data as string;
-       this.imgPre = 'http://127.0.0.1:5000/image/'+data
+       this.imgPre = 'http://203.150.243.147:5000/image/'+data
     })
   }
 
